c363814b085c8454fcf1131790a99b5a53ece8bd
sumit_dhandar <sumitsdhandar@gmail.com>
2025-05-26 15:48:08 +0530

    added comment for annotation

--- src/main/java/net/javaguides/springboot/exception/EmailAlreadyExistsException.java
+++ src/main/java/net/javaguides/springboot/exception/EmailAlreadyExistsException.java
@@ -3,7 +3,7 @@ package net.javaguides.springboot.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST) //no need to use this annotation if we are using global handler
 public class EmailAlreadyExistsException extends RuntimeException{
 private String message;
 public EmailAlreadyExistsException(String message){
